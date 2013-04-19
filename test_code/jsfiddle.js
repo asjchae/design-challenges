2e6b3c0fcd626479296d5d93e3bc3766c621e2b0
achae <achae@ubuntu.ubuntu-domain>
2013-04-19 15:58:47 -0400

    Initial commit

---
+++ test_code/jsfiddle.js
@@ -0,0 +1,8 @@
+//Stuff that TCR put into JSFIDDLE.
+//This is the JS code for the iframe that lets the parent talk to the child, etc.
+
+$('iframe')[0].contentDocument.body.innerHTML = '<scri' + 'pt>console.log(window);</scr' + 'ipt>hi';
+
+$('button').on('click', function () {
+   EVALCHILD($('textarea').val());
+});
