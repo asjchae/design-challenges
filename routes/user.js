38102caa41234feece4cc633d68886ebb718b13f
achae <achae@ubuntu.ubuntu-domain>
2013-04-23 15:45:50 -0400

    Scrapping everyauth

--- routes/user.js
+++ routes/user.js
@@ -10,24 +10,7 @@ exports.list = function(req, res){
 };
 
 exports.login = function(req, res) {
-
-// Facebook
-everyauth.facebook
-  .appId(process.env.FB_ID)
-  .appSecret(process.env.FB_SECRET)
-  .handleAuthCallbackError( function (req, res) {
-    // If a user denies your app, Facebook will redirect the user to
-    // /auth/facebook/callback?error_reason=user_denied&error=access_denied&error_description=The+user+denied+your+request.
-    // This configurable route handler defines how you want to respond to
-    // that.
-    // If you do not configure this, everyauth renders a default fallback
-    // view notifying the user that their authentication failed and why.
-  })
-  .findOrCreateUser( function (session, accessToken, accessTokExtra, fbUserMetadata) {
-    // find or create user logic goes here
-  })
-  .redirectPath('/');
-
+  res.send("log in");
     // req.session.url = '/fetch';
 	// res.render('login', {title: "Sign In"});
 };
