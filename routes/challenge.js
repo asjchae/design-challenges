5130b8df29ee69dcc89c43549cce13a768b40b68
skinc <shane.skikne@students.olin.edu>
2013-04-25 23:55:03 -0400

    Added challenges

--- routes/challenge.js
+++ routes/challenge.js
@@ -1,19 +1,26 @@
 var  Team = require('../models/teammodel')
-    , Challenge = require('../models/projectmodel')
+    , Challenge = require('../models/challengemodel')
     , mongoose = require('mongoose');
 
 
 exports.addchallenge = function(req, res){
-    res.send("Needs to be implemented");
+    res.render('addchallenge', {title: "Log In"});
 };
 
 exports.addchallengepost = function(req, res){
-  
-    var team = new Challenge({name: req.body.name, type: req.body.type, 
-                        promt: req.body.prompt, description: req.body.description, 
+    console.log(req.body) 
+    var challenge = new Challenge({name: req.body.name, type: req.body.type, 
+                        prompt: req.body.prompt, description: req.body.description, 
                         contactname: req.body.contactname, email: req.body.email});
+    challenge.save(function (err) {
+        if (err) {
+            console.log("Problem signing team up", err);
 
-    res.send("Needs to be implemented");
+            res.send("WOw there friend, you may need to slow down!");
+        } else {
+            res.send("You're challenge is added! " + req.body.name + "!");
+        }
+    });
 };
 
 exports.challengebrowser = function(req, res){
