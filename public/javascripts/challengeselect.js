87ea575154a10e521506ac93ab64108959d46ac9
skinc <shane.skikne@students.olin.edu>
2013-05-13 09:44:40 -0400

    Hi

--- public/javascripts/challengeselect.js
+++ public/javascripts/challengeselect.js
@@ -1,17 +1,42 @@
+
+
 $(function(){
 
+     $(".challengeselect").click( function (evt) {
+
+
+        console.log("called!")
+
+        $.post("/selectchallenge", {projectname: this.value}, function (data) {
+
+         window.location = data.redirect;
+
+        });
+     });
      $(".challenge").hover( function (evt) {
-     	$(evt.target).find(".hidden").show()
+
+      $(evt.target).find(".hidden").show()
+
      })
      $(".challenge").mouseover( function (evt) {
+
         $(evt.target).find(".hidden").show()
+
      })
 
      $(".challenge").mouseleave( function (evt) {
+
         $(evt.target).find(".hidden").hide()
+
      })
 
      $(".hidden").mouseleave( function (evt) {
+
         $(evt.target).find(".hidden").hide()
+
      })
 })
+
+ 
+    
+    
